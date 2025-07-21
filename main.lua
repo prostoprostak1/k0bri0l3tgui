9e2522baa0ce54c1031c7c18aaa605ef35c80de1
prostoprostak1 <w3rrix1337@gmail.com>
2025-07-21 17:07:59 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,99 @@
+                                                                                 local ScreenGui=      
+                                                                        Instance.new("ScreenGui");local Frame=Instance. 
+                                                                    new("Frame");local Frame_2=Instance.new("Frame");local        
+                                                                TextLabel=Instance.new("TextLabel");local UICorner=Instance.new(        
+                                                            "UICorner");local TextButton=Instance.new("TextButton");local TextButton_2=   
+                                                          Instance.new("TextButton");local UICorner_2=Instance.new("UICorner");local        
+                                                        TextButton_3=Instance.new("TextButton");local UICorner_3=Instance.new("UICorner");    
+                                                      local TextButton_4=Instance.new("TextButton");local UICorner_4=Instance.new("UICorner");  
+                                                    local TextButton_5=Instance.new("TextButton");local UICorner_5=Instance.new("UICorner");local 
+                                                   TextButton_6=Instance.new("TextButton");local UICorner_6=Instance.new("UICorner");local          
+                                                  TextButton_7=Instance.new("TextButton");local UICorner_7=Instance.new("UICorner");local             
+                                                TextButton_8=Instance.new("TextButton");local UICorner_8=Instance.new("UICorner");local TextButton_9=   
+                                                Instance.new("TextButton");local UICorner_9=Instance.new("UICorner");local TextButton_10=Instance.new(    
+                                              "TextButton");local UICorner_10=Instance.new("UICorner");local UICorner_11=Instance.new("UICorner");local     
+                                              TextButton_11=Instance.new("TextButton");local UICorner_12=Instance.new("UICorner");ScreenGui.Parent=game.    
+                                            Players.LocalPlayer:WaitForChild("PlayerGui");ScreenGui.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;Frame.Parent=  
+                                            ScreenGui;Frame.BackgroundColor3=Color3.fromRGB(45,45,45);Frame.BorderColor3=Color3.fromRGB(0,0,0);Frame.           
+                                          BorderSizePixel=0;Frame.Position=UDim2.new(0.44974944,0,0.374371856,0);Frame.Size=UDim2.new(0,571,0,200);Frame_2.Parent 
+                                          =Frame;Frame_2.BackgroundColor3=Color3.fromRGB(0,0,0);Frame_2.BorderColor3=Color3.fromRGB(0,0,0);Frame_2.BorderSizePixel= 
+                                          0;Frame_2.Position=UDim2.new(0,0, -0.00499999989,0);Frame_2.Size=UDim2.new(0,571,0,41);TextLabel.Parent=Frame_2;TextLabel.  
+                                          BackgroundColor3=Color3.fromRGB(255,255,255);TextLabel.BackgroundTransparency=1;TextLabel.BorderColor3=Color3.fromRGB(0,0,0 
+                                        );TextLabel.BorderSizePixel=0;TextLabel.Position=UDim2.new(0.0420315228,0, -0.121951222,0);TextLabel.Size=UDim2.new(0,200,0,50) 
+                                        ;TextLabel.Font=Enum.Font.FredokaOne;TextLabel.Text="k0bri0l3t gui "; --[[==============================]]TextLabel.TextColor3=   
+                                        Color3.fromRGB(255,255,255);TextLabel.TextSize=45;UICorner. --[[============================================]]Parent=Frame_2;     
+                                        TextButton.Parent=Frame_2;TextButton.BackgroundColor3=  --[[======================================================]]Color3.fromRGB( 
+                                      255,255,255);TextButton.BackgroundTransparency=1;     --[[==========================================================]]TextButton.       
+                                      BorderColor3=Color3.fromRGB(0,0,0);TextButton.      --[[==============================================================]]BorderSizePixel 
+                                      =0;TextButton.Position=UDim2.new(0.91243434,0,0,0); --[[================================================================]]TextButton.Size 
+                                      =UDim2.new(0,50,0,45);TextButton.Font=Enum.Font.    --[[==================================================================]]              
+                                      SourceSansBold;TextButton.Text="X";TextButton.      --[[==================================================================]]TextColor3=       
+                                    Color3.fromRGB(255,255,255);TextButton.TextSize=30;   --[[====================================================================]]TextButton_2. 
+                    Parent=Frame;TextButton_2.BackgroundColor3=Color3.fromRGB(255,255,255 --[[====================================================================]]);TextButton_2. 
+              BorderColor3=Color3.fromRGB(0,0,0);TextButton_2.BorderSizePixel=0;          --[[======================================================================]]TextButton_2. 
+            Position=UDim2.new(0.0227669682,0,0.712861657,0);TextButton_2.Size=UDim2.new( --[[======================================================================]]0,119,0,42);  
+          TextButton_2.Font=Enum.Font.FredokaOne;TextButton_2.Text="Jerk off";            --[[======================================================================]]TextButton_2. 
+        TextColor3=Color3.fromRGB(0,0,0);TextButton_2.TextSize=25;UICorner_2.Parent=      --[[======================================================================]]TextButton_2; 
+        TextButton_3.Parent=Frame;TextButton_3.BackgroundColor3=Color3.fromRGB(255,255,   --[[======================================================================]]255);         
+      TextButton_3.BorderColor3=Color3.fromRGB(0,0,0);TextButton_3.BorderSizePixel=0;     --[[======================================================================]]TextButton_3. 
+      Position=UDim2.new(0.765323877,0,0.446100622,0);TextButton_3.Size=UDim2.new(0,119,0,  --[[==================================================================]]42);            
+      TextButton_3.Font=Enum.Font.FredokaOne;TextButton_3.Text="Invis gui";TextButton_3.    --[[================================================================]]TextColor3=Color3 
+    .fromRGB(0,0,0);TextButton_3.TextSize=25;UICorner_3.Parent=TextButton_3;TextButton_4.   --[[==============================================================]]Parent=Frame;     
+    TextButton_4.BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_4.BorderColor3=      --[[==========================================================]]Color3.fromRGB(0,0, 
+    0);TextButton_4.BorderSizePixel=0;TextButton_4.Position=UDim2.new(0.535901845,0,0.446100622 --[[====================================================]],0);TextButton_4.Size=  
+    UDim2.new(0,119,0,42);TextButton_4.Font=Enum.Font.FredokaOne;TextButton_4.Text="Fling gui";   --[[==============================================]]TextButton_4.TextColor3=  
+    Color3.fromRGB(0,0,0);TextButton_4.TextSize=25;UICorner_4.Parent=TextButton_4;TextButton_5.Parent --[[====================================]]=Frame;TextButton_5.          
+    BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_5.BorderColor3=Color3.fromRGB(0,0,0);         --[[========================]]TextButton_5.BorderSizePixel=0;       
+    TextButton_5.Position=UDim2.new(0.294220567,0,0.446100622,0);TextButton_5.Size=UDim2.new(0,119,0,42);TextButton_5.Font=Enum.Font.FredokaOne;TextButton_5.Text="RC7";    
+  TextButton_5.TextColor3=Color3.fromRGB(0,0,0);TextButton_5.TextSize=25;UICorner_5.Parent=TextButton_5;TextButton_6.Parent=Frame;TextButton_6.BackgroundColor3=Color3.   
+  fromRGB(255,255,255);TextButton_6.BorderColor3=Color3.fromRGB(0,0,0);TextButton_6.BorderSizePixel=0;TextButton_6.Position=UDim2.new(0.0192644484,0,0.446100622,0);    
+  TextButton_6.Size=UDim2.new(0,138,0,42);TextButton_6.Font=Enum.Font.FredokaOne;TextButton_6.Text="Infinite yield";TextButton_6.TextColor3=Color3.fromRGB(0,0,0);        
+  TextButton_6.TextSize=25;UICorner_6.Parent=TextButton_6;TextButton_7.Parent=Frame;TextButton_7.BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_7.BorderColor3=  
+  Color3.fromRGB(0,0,0);TextButton_7.BorderSizePixel=0;TextButton_7.Position=UDim2.new(0.744308114,0,0.203050315,0);TextButton_7.Size=UDim2.new(0,119,0,42);TextButton_7. 
+  Font=Enum.Font.FredokaOne;TextButton_7.Text="Fly gui";TextButton_7.TextColor3=Color3.fromRGB(0,0,0);TextButton_7.TextSize=25;UICorner_7.Parent=TextButton_7;            
+  TextButton_8.Parent=Frame;TextButton_8.BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_8.BorderColor3=Color3.fromRGB(0,0,0);TextButton_8.BorderSizePixel=0;     
+  TextButton_8.Position=UDim2.new(0.502626956,0,0.203050315,0);TextButton_8.Size=UDim2.new(0,119,0,42);TextButton_8.Font=Enum.Font.FredokaOne;TextButton_8.Text=          
+  "Part orbit";TextButton_8.TextColor3=Color3.fromRGB(0,0,0);TextButton_8.TextSize=25;UICorner_8.Parent=TextButton_8;TextButton_9.Parent=Frame;TextButton_9.              
+  BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_9.BorderColor3=Color3.fromRGB(0,0,0);TextButton_9.BorderSizePixel=0;TextButton_9.Position=UDim2.new(0.260945708 
+  ,0,0.203050315,0);TextButton_9.Size=UDim2.new(0,119,0,42);TextButton_9.Font=Enum.Font.FredokaOne;TextButton_9.Text="Decal spam";TextButton_9.TextColor3=Color3.fromRGB( 
+  0,0,0);TextButton_9.TextSize=25;UICorner_9.Parent=TextButton_9;TextButton_10.Parent=Frame;TextButton_10.BackgroundColor3=Color3.fromRGB(255,255,255);TextButton_10.       
+  BorderColor3=Color3.fromRGB(0,0,0);TextButton_10.BorderSizePixel=0;TextButton_10.Position=UDim2.new(0.0210157614,0,0.203050315,0);TextButton_10.Size=UDim2.new(0,119,0,42 
+  );TextButton_10.Font=Enum.Font.FredokaOne;TextButton_10.Text="Skybox";TextButton_10.TextColor3=Color3.fromRGB(0,0,0);TextButton_10.TextSize=25;UICorner_10.Parent=        
+  TextButton_10;UICorner_11.Parent=Frame;TextButton_11.Parent=ScreenGui;TextButton_11.BackgroundColor3=Color3.fromRGB(0,0,0);TextButton_11.BorderColor3=Color3.fromRGB(0,0, 
+  0);TextButton_11.BorderSizePixel=0;TextButton_11.Position=UDim2.new(0.00805057026,0,0.437185943,0);TextButton_11.Size=UDim2.new(0,100,0,100);TextButton_11.Font=Enum.Font 
+  .SourceSans;TextButton_11.Text="K0B";TextButton_11.TextColor3=Color3.fromRGB(255,255,255);TextButton_11.TextSize=50;UICorner_12.Parent=TextButton_11;local function       
+  OOYYD_fake_script() local script=Instance.new("Script",TextButton);local gui=script.Parent.Parent.Parent;local knopka=script.Parent;script.Parent.MouseButton1Click:      
+  Connect(function() gui.Visible=false;gui.UIDragDetector.Destroy();end);end coroutine.wrap(OOYYD_fake_script)();local function FVIQBDN_fake_script() local script=Instance 
+  .new("Script",TextButton_2);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))();end);   
+  end coroutine.wrap(FVIQBDN_fake_script)();local function KKUO_fake_script() local script=Instance.new("Script",TextButton_3);local knopka=script.Parent;knopka.           
+  MouseButton1Click:Connect(function() loadstring(game:HttpGet("https://pastebin.com/raw/3Rnd9rHf"))();end);end coroutine.wrap(KKUO_fake_script)();local function           
+  QMZME_fake_script() local script=Instance.new("Script",TextButton_4);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() loadstring(game:HttpGet(      
+  "https://pastebin.com/raw/VynRhxJV",true))();end);end coroutine.wrap(QMZME_fake_script)();local function XTMOA_fake_script() local script=Instance.new("Script",          
+  TextButton_5);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() loadstring(game:HttpGet(                                                           
+  "https://raw.githubusercontent.com/CoreGui/Scripts/main/RC7"))();end);end coroutine.wrap(XTMOA_fake_script)();local function OKIVV_fake_script() local script=Instance. 
+  new("Script",TextButton_6);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() loadstring(game:HttpGet(                                              
+    "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))();end);end coroutine.wrap(OKIVV_fake_script)();local function GAXOVEH_fake_script() local    
+    script=Instance.new("Script",TextButton_7);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() getgenv().script=                                   
+    "https://raw.githubusercontent.com/danya2854/Myscripts/refs/heads/main/Fly";loadstring(game:HttpGet(                                                                  
+    "https://raw.githubusercontent.com/danya2854/Myscripts/refs/heads/main/loader"))();end);end coroutine.wrap(GAXOVEH_fake_script)();local function GUOOC_fake_script()  
+      local script=Instance.new("Script",TextButton_8);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() loadstring(game:HttpGet(                  
+      "https://pastebin.com/raw/aZjaAr6F",true))();end);end coroutine.wrap(GUOOC_fake_script)();local function YUIGKTC_fake_script() local script=Instance.new("Script" 
+      ,TextButton_9);local knopka=script.Parent;knopka.MouseButton1Click:Connect(function() local decalID=122859041362655;local function exPro(root) for _,v in pairs(  
+        root:GetChildren()) do if (v:IsA("Decal") and (v.Texture~=("http://www.roblox.com/asset/?id="   .. decalID))) then v.Parent=nil;elseif v:IsA("BasePart") then v 
+        .Material="Plastic";v.Transparency=0;local One=Instance.new("Decal",v);local Two=Instance.new("Decal",v);local Three=Instance.new("Decal",v);local Four=        
+        Instance.new("Decal",v);local Five=Instance.new("Decal",v);local Six=Instance.new("Decal",v);One.Texture="http://www.roblox.com/asset/?id="   .. decalID ;Two.  
+          Texture="http://www.roblox.com/asset/?id="   .. decalID ;Three.Texture="http://www.roblox.com/asset/?id="   .. decalID ;Four.Texture=                       
+            "http://www.roblox.com/asset/?id="   .. decalID ;Five.Texture="http://www.roblox.com/asset/?id="   .. decalID ;Six.Texture=                               
+              "http://www.roblox.com/asset/?id="   .. decalID ;One.Face="Front";Two.Face="Back";Three.Face="Right";Four.Face="Left";Five.Face="Top";Six.Face="Bottom" 
+                ;end exPro(v);end end local function asdf(root) for _,v in pairs(root:GetChildren()) do asdf(v);end end exPro(game.Workspace);asdf(game.Workspace);   
+                  end);end coroutine.wrap(YUIGKTC_fake_script)();local function XMKPJGN_fake_script() local script=Instance.new("Script",TextButton_10);local       
+                      knopka=script.Parent;knopka.MouseButton1Click:Connect(function() local s=Instance.new("Sky");s.Name="Sky";s.Parent=game.Lighting;local        
+                                  skyboxID=122859041362655;s.SkyboxBk="http://www.roblox.com/asset/?id="   .. skyboxID ;s.SkyboxDn=                                 
+                                      "http://www.roblox.com/asset/?id="   .. skyboxID ;s.SkyboxFt="http://www.roblox.com/asset/?id="   .. skyboxID ;s.SkyboxLf=    
+                                      "http://www.roblox.com/asset/?id="   .. skyboxID ;s.SkyboxRt=         "http://www.roblox.com/asset/?id="   .. skyboxID ;s.    
+                                      SkyboxUp="http://www.roblox.com/asset/?id="   .. skyboxID ;           game.Lighting.TimeOfDay=12;while true do game.        
+                                      Lighting.Ambient=Color3.new(math.random(),math.random(),math.         random());wait(0.2);game.Lighting.ShadowColor=Color3. 
+                                      new(math.random(),math.random(),math.random());wait(0.2);end          end);end coroutine.wrap(XMKPJGN_fake_script)();local  
+                                      function QOCA_fake_script() local script=Instance.new(                  "Script",TextButton_11);local gui=script.Parent.    
+                                      Parent:WaitForChild("Frame");local knopka=script.Parent;                knopka.MouseButton1Click:Connect(function() gui.    
+                                      Visible=false;gui.Visible=true;Instance.new("UIDragDetector")           ;end);end coroutine.wrap(QOCA_fake_script)();
